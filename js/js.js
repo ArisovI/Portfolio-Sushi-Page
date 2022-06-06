30a66becb179ea983adcd17bc0829089a6555e51
ArisovI <106986227+ArisovI@users.noreply.github.com>
2022-06-06 20:52:39 +0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,231 @@
+$(document).ready(function () {
+  $('.main-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    nextArrow: '<i class="icon-arrow-right" aria-hidden="true"></i>',
+    prevArrow: '<i class="icon-arrow-left" aria-hidden="true"></i>',
+    dots: true,
+    dotsClass: 'main-dots',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('.sets-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: '<i class="icon-arrow-right2"" aria-hidden="true"></i>',
+    prevArrow: '<i class="icon-arrow-left2" aria-hidden="true"></i>',
+    dots: true,
+    dotsClass: 'sets-dots',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: true
+
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          infinite: true,
+          dots: false,
+          centerMode: true,
+          centerPadding: '30px'
+
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('.rolls-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: '<i class="icon-arrow-right2"" aria-hidden="true"></i>',
+    prevArrow: '<i class="icon-arrow-left2" aria-hidden="true"></i>',
+    dots: true,
+    dotsClass: 'rolls-dots',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: true
+
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          infinite: true,
+          dots: false,
+          centerMode: true,
+          centerPadding: '30px'
+
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('.pizza-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: '<i class="icon-arrow-right2"" aria-hidden="true"></i>',
+    prevArrow: '<i class="icon-arrow-left2" aria-hidden="true"></i>',
+    dots: true,
+    dotsClass: 'pizza-dots',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: true
+
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          infinite: true,
+          dots: false,
+          centerMode: true,
+          centerPadding: '30px'
+
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('.wok-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: '<i class="icon-arrow-right2"" aria-hidden="true"></i>',
+    prevArrow: '<i class="icon-arrow-left2" aria-hidden="true"></i>',
+    dots: true,
+    dotsClass: 'wok-dots',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: true
+
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          infinite: true,
+          dots: false,
+          centerMode: true,
+          centerPadding: '30px'
+
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('.stook-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: '<i class="icon-arrow-right2"" aria-hidden="true"></i>',
+    prevArrow: '<i class="icon-arrow-left2" aria-hidden="true"></i>',
+    dots: true,
+    dotsClass: 'stook-dots',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: true
+
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          infinite: true,
+          dots: false,
+          centerMode: true,
+          centerPadding: '30px'
+
+        }
+      }
+    ]
+  });
+});
